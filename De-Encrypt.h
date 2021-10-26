216336b4586573ca289bd3ae05558bd54eee9fb0
BOBBIJDJ <76691110+BOBBIJDJ@users.noreply.github.com>
2021-10-26 18:46:08 +0200

    changes

--- De-Encrypt.h
+++ De-Encrypt.h
@@ -1,25 +1,36 @@
-#include <string>
+void CrypScrFn(int CrntNumScr,char CrntCharScr[][6], int Difficulty) {
+  
+  int i;
+  int ReversedScore = 0;
+  int n = sizeof(CrntNumScr);
 
-void CrypScrFn(int CrntNumScr,char CrntCharScr[]) {
-  
-  int k;
-  
-  for (k=0; k<=3; k++) {
-    
-    CrntCharScr[k] = (CrntNumScr % 10)+33;
-    
+  for (i=0; i<=n; i++) {
+
+    ReversedScore = (ReversedScore * 10) + (CrntNumScr % 10);
+
     CrntNumScr /= 10;
+
+  }
+
+  for (i=0; i<=n; i++) {
+    
+    CrntCharScr[Difficulty][i] = (ReversedScore % 10) + 33;
+
+    ReversedScore /= 10;
 
   }
 }
 
-void DecrypHiScrFn(std::string CrypHiScr, int DecrypHiScr[]) {
+void DecrypHiScrFn(char HiScrCryp[][6], char DecrypHiScr[][4]) {
   
-  int k;
+  int i, j;
 
-  for (k=0; k<(sizeof(CrypHiScr)/sizeof(CrypHiScr[0])); k++) {
+  for (i=0; i<5; i++) {
 
-    DecrypHiScr[k] = (CrypHiScr[k] - 33);
+    for (j=0; j<4 && HiScrCryp[i][j] != 10; j++) {
 
+      DecrypHiScr[i][j] = (HiScrCryp[i][j] + 15);
+    
+    }
   }
 }
--- main.cpp
+++ main.cpp
@@ -8,55 +8,82 @@
 int main()
 {
   
-  int CrntNumScr, HiScr[5][4], MinMaxNum[2] = {0,0}, play = 1, i, j, GssDiff;
-  char CrntCharScr[4];
-  std::string HiScrChar;
+  int CrntNumScr, MinMaxNum[2] = {0,0}, play = 1, i, j, GssDiff, IntSelDiffHiScr;
+  char CryptedHiScr[5][6], DecryptedHiScr[5][4], CharSelDiffHiScr[4];
 
   for (i=0; i<5; i++) {
-    for (j=0; j<4; j++) {
-      HiScr[i][j]=0;
+    for (j=0; j<6; j++) {
+      DecryptedHiScr[i][j] = 'E';
+      CryptedHiScr[i][j] = '\0';
     }
+    CharSelDiffHiScr[i] = '\0';
   }
   
   std::fstream HiScrFile;
-  HiScrFile.open("HighScore.HGSC", std::ios::in);
-
-  for (i=0; i<=4; i++) {
-    getline(HiScrFile, HiScrChar);
-
-    DecrypHiScrFn(HiScrChar,/*decrypted array to return*/);
-  }
 
   while (play == 1) {
   
+    HiScrFile.open("HighScore.HGSC", std::ios::in);
+
+    for (i=0; i<5; i++) {
+    
+      for (j=0; j<6 && CryptedHiScr[i][j-1]!=10; j++) {
+      
+        CryptedHiScr[i][j] = HiScrFile.get();
+
+      }
+    }
+
+    HiScrFile.close();
+
+    DecrypHiScrFn(CryptedHiScr,DecryptedHiScr);
+
     std::cout << "Welcome to Guess the number!\n\n";
     
     GssDiff = SelDiffFn(MinMaxNum);
 
-    std::cout << "your high score for this difficulty is " << HiScr[GssDiff] << "\n\n";
+    for (i=0; i<4 && DecryptedHiScr[GssDiff][i] != 69; i++) {
+      CharSelDiffHiScr[i] = DecryptedHiScr[GssDiff][i];
+    }
+
+    IntSelDiffHiScr = std::atoi(CharSelDiffHiScr);
+
+    std::cout << "your high score for this difficulty is: ";
+    
+    for (i=0; i<4 && DecryptedHiScr[GssDiff][i] != 69; i++) {
+      std::cout << DecryptedHiScr[GssDiff][i];
+    }
+
+    std::cout << "\n\n";
   
     CrntNumScr = GssNumFn(MinMaxNum[0], MinMaxNum[1]);
     
     std::cout << "your score is: " << CrntNumScr << " \n\n";
+
+    if (CrntNumScr > IntSelDiffHiScr) {
+      std::cout << "Well Played, you've got a new High score\n\n";
+
+      CrypScrFn(CrntNumScr, CryptedHiScr, GssDiff);
+
+      HiScrFile.open("HighScore.HGSC", std::ios::out);
+      
+      for (i=0; i<5; i++) {
+        for (j=0; j<6 && CryptedHiScr[i][j] != 0; j++) {
+          HiScrFile.put(CryptedHiScr[i][j]);
+        }
+        HiScrFile.put('\n');
+      }
+
+      HiScrFile.close();
+    }
+    else {
+      std::cout << "nice game but not enough, you didn't get a new High score\n\n";
+    }
     
     std::cout << "would you like to play again?\n";
     std::cout << "0 - close the game\n";
     std::cout << "1 - try again\n";
     scanf("%d", &play);
-    
-    CrypScrFn(CrntNumScr, CrntCharScr);
-    
-    for (i=0; i<=3; i++) {
-      
-      std::cout <<  CrntCharScr[i];
-      
-    }
-    
-   /* if (CrntNumScr > HiScr[GssDiff]) {
-    
-    } */
-    
-  } 
-
-    return 0;
+  }
+  return 0;
 }
